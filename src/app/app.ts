8ba373b0934a37cb2ea99751774aace41de9972f
Bryan <bryanparcival@gmail.com>
2026-03-03 16:14:08 +0100

    added overview component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [OverviewComponent],
 })
-export class App {
-  protected readonly title = signal('todo-client');
-}
+export class App {}
